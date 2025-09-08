c956a5eca90a7e83b0dfd545bc63e55c418502bc
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 04:03:30 +0600

    CategoryListView urls  create

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views.catagory import CategoryListView
 
 urlpatterns = [
-    
+    path(
+        'categories/',
+        CategoryListView.as_view(),
+        name='category-list'
+    ),
 ]
