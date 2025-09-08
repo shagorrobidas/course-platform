ead6c92c2a327140045e56b434f5749315fdf1e7
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 04:08:52 +0600

    CourseListView create

---
+++ courses/api/views/courses.py
@@ -0,0 +1,45 @@
+from rest_framework import generics, permissions
+from django.db.models import Q
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from courses.models import (
+    Enrollment,
+    Module,
+    Lesson,
+    Progress,
+    Course
+)
+from courses.api.serializers import CourseSerializer
+
+
+class CourseListView(generics.ListAPIView):
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        queryset = Course.objects.filter(is_published=True)
+
+        # Filter by category
+        category = self.request.query_params.get('category', None)
+        if category:
+            queryset = queryset.filter(category__name=category)
+
+        # Filter by level
+        level = self.request.query_params.get('level', None)
+        if level:
+            queryset = queryset.filter(level=level)
+
+        # Search
+        search = self.request.query_params.get('search', None)
+        if search:
+            queryset = queryset.filter(
+                Q(
+                    title__icontains=search
+                ) | Q(description__icontains=search)
+            )
+
+        return queryset
+
+
+
+
