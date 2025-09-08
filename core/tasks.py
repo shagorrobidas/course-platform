f9a9b2439496417f47a64d0ff1e29770a1d43ac6
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 03:53:32 +0600

    sent welcome email task

---
+++ core/tasks.py
@@ -0,0 +1,32 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+import os
+from io import BytesIO
+from django.core.files.base import ContentFile
+from courses.models import Enrollment
+
+
+@shared_task
+def send_welcome_email(user_id):
+    from users.models import User
+    try:
+        user = User.objects.get(id=user_id)
+        subject = 'Welcome to Our Course Platform'
+        message = render_to_string('emails/welcome_email.html', {
+            'user': user
+        })
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+        )
+    except User.DoesNotExist:
+        pass
+
