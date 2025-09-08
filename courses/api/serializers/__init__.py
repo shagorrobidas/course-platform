88e1e7d388243540867a900b291955aeadaefc21
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 03:10:43 +0600

    init file

--- courses/api/serializers/__init__.py
+++ courses/api/serializers/__init__.py
@@ -1,8 +1,16 @@
 from courses.api.serializers.catagory import CategorySerializer
 from courses.api.serializers.lesson import LessonSerializer
+from courses.api.serializers.module import ModuleSerializer
+from courses.api.serializers.courses import CourseSerializer
+from courses.api.serializers.enrollment import EnrollmentSerializer
+from courses.api.serializers.progress import ProgressSerializer
 
 
 __all__ = (
     CategorySerializer,
     LessonSerializer,
+    ModuleSerializer,
+    CourseSerializer,
+    EnrollmentSerializer,
+    ProgressSerializer
 )
