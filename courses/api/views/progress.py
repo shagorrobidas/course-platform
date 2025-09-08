e6a327013d66ed74558a65dfef6b917baa982ce2
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 04:55:14 +0600

    ProgressUpdateView create

---
+++ courses/api/views/progress.py
@@ -0,0 +1,49 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from courses.models import Progress, Enrollment, Lesson
+from courses.api.serializers import ProgressSerializer
+from core.tasks import generate_certificate
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+
+
+class ProgressUpdateView(generics.UpdateAPIView):
+    serializer_class = ProgressSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Progress.objects.filter(student=user)
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.completed = request.data.get('completed', instance.completed)
+        instance.save()
+
+        # Check if course is completed
+        enrollment = Enrollment.objects.get(
+            student=request.user, 
+            course=instance.lesson.module.course
+        )
+
+        if not enrollment.completed:
+            total_lessons = Lesson.objects.filter(
+                module__course=enrollment.course
+            ).count()
+            completed_lessons = Progress.objects.filter(
+                student=request.user,
+                lesson__module__course=enrollment.course,
+                completed=True
+            ).count()
+            if completed_lessons == total_lessons:
+                enrollment.completed = True
+                enrollment.save()
+
+                # Trigger certificate generation
+                generate_certificate.delay(enrollment.id)
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+
