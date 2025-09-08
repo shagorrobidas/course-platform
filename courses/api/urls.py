3a8512283ab4beb097e40fc998cae15511fa08ad
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 04:10:35 +0600

    CourseListView urls create

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views.catagory import CategoryListView
+from .views.courses import CourseListView
 
 urlpatterns = [
     path(
         'categories/',
         CategoryListView.as_view(),
         name='category-list'
+    ),
+    path(
+        '',
+        CourseListView.as_view(),
+        name='course-list'
     ),
 ]
