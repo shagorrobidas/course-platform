aed0528f7c05651b275833c28fb69fec27702ae3
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 01:39:13 +0600

    courses api

---
+++ courses/api/serializers/__init__.py
@@ -0,0 +1,8 @@
+from courses.api.serializers.catagory import CategorySerializer
+from courses.api.serializers.lesson import LessonSerializer
+
+
+__all__ = (
+    CategorySerializer,
+    LessonSerializer,
+)
