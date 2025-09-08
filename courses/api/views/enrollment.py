5f8b0ce565f959fa6e96bb9b0efd7ec803d8af19
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 04:32:47 +0600

    EnrollmentCreateView create

---
+++ courses/api/views/enrollment.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, permissions
+from courses.models import Enrollment, Course
+from courses.api.serializers import EnrollmentSerializer
+
+
+class EnrollmentCreateView(generics.CreateAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        course_id = self.kwargs['course_id']
+        course = Course.objects.get(id=course_id)
+        serializer.save(
+            student=self.request.user,
+            course=course
+        )
+
+
