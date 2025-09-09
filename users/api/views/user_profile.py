775d6893261734f6e4e7aae95227b3d54d4ba4be
Shagor Robidas <roysagor88@gmail.com>
2025-09-09 23:29:21 +0600

    user profile update

--- users/api/views/user_profile.py
+++ users/api/views/user_profile.py
@@ -1,10 +1,38 @@
 from rest_framework import generics, permissions
 from users.api.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class UserProfileView(generics.RetrieveUpdateAPIView):
+class UserProfileView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAuthenticated,)
-    
+
     def get_object(self):
         return self.request.user
+
+
+class UserProfileUpdateView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Profile updated successfully",
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
+    
+
