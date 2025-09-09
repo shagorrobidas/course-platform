47bf0c3c2e08a28b162c96c6ffaecda96a61ec18
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 00:29:20 +0600

    ResetPasswordConfirmSerializer Create

--- users/api/serializers/reset_password.py
+++ users/api/serializers/reset_password.py
@@ -5,4 +5,13 @@ class ResetPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
 
 
+class ResetPasswordConfirmSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    otp = serializers.CharField(max_length=6, required=True)
+    new_password = serializers.CharField(required=True, min_length=6)
+    confirm_password = serializers.CharField(required=True)
 
+    def validate(self, data):
+        if data['new_password'] != data['confirm_password']:
+            raise serializers.ValidationError("New passwords don't match")
+        return data
