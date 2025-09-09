93f06acfa3b4960a9ab6cdab5743db04148f45b3
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 03:46:00 +0600

    id add

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from .models import User, EmailVerification, OTP
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'is_email_verified')
+    list_display = ('id', 'email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'is_email_verified')
     search_fields = ('email', 'first_name', 'last_name')
     list_filter = ('role', 'is_active', 'is_staff', 'is_email_verified')
     ordering = ('email',)
