2c26f34b497e97b103132eac406be0f181d6a294
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 03:32:25 +0600

    bug fix

--- users/tasks.py
+++ users/tasks.py
@@ -1,21 +1,21 @@
 from celery import shared_task
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.conf import settings
 from .utils import send_otp_email, send_verification_email
+from users.models import User
+
 
 @shared_task
 def send_otp_email_task(user_id, otp, purpose):
-    from .models import User
     try:
         user = User.objects.get(id=user_id)
         send_otp_email(user, otp, purpose)
+        print(f"Sent OTP email to {user.email} for {purpose} with OTP: {otp}")  # For debugging; remove in production
     except User.DoesNotExist:
         pass
 
+
 @shared_task
 def send_verification_email_task(user_id, verification_url):
-    from .models import User
+
     try:
         user = User.objects.get(id=user_id)
         send_verification_email(user, verification_url)
