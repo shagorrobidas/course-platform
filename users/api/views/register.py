58d7ca2a1911136b2c8c1bbaf9ecd5875910f70d
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 00:46:06 +0600

    update responce massage

--- users/api/views/register.py
+++ users/api/views/register.py
@@ -8,17 +8,21 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
-    
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        
+
         # User is created but not active until email is verified
-        user.is_active = False
-        user.save()
-        
+        if user.role != "admin":
+            user.is_active = False
+            user.save()
+            message = "User registered successfully. Please verify your email."
+        else:
+            message = "Admin user registered successfully."
+
         return Response({
-            'message': 'User registered successfully. Please verify your email.',
-            'user': UserSerializer(user).data
+            'message': message,
+            # 'user': UserSerializer(user).data
         }, status=status.HTTP_201_CREATED)
