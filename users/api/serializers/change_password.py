ae8d59c7f97960acc0330cad5b67e202e6ccab8d
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 00:19:27 +0600

    change password serializer crate

---
+++ users/api/serializers/change_password.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, min_length=6)
+    confirm_password = serializers.CharField(required=True)
+    
+    def validate(self, data):
+        if data['new_password'] != data['confirm_password']:
+            raise serializers.ValidationError("New passwords don't match")
+        return data
