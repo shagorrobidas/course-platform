996c6fbdf843eefc4b8ae5bfb0ddb58120358a90
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 00:27:28 +0600

    RegisterSerializer Create

---
+++ users/api/serializers/register.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password',
+            'password_confirm',
+            'first_name',
+            'last_name',
+            'role'
+        )
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            role=validated_data.get('role', 'student')
+        )
+        return user
