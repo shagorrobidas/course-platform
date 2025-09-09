b35908463890214dc4c47ccaf7d689b038eaec36
Shagor Robidas <roysagor88@gmail.com>
2025-09-10 03:29:56 +0600

    token veryfi massage comment

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
+# from django.conf import settings
 from .models import User, EmailVerification
 from .utils import send_verification_email, send_otp_email
 from django.utils import timezone
@@ -21,8 +21,8 @@ def create_email_verification(sender, instance, created, **kwargs):
             expires_at=expires_at
         )
         
-        verification_url = f"{settings.FRONTEND_URL}/verify-email/{token}/"
-        send_verification_email(instance, verification_url)
+        # verification_url = f"{settings.FRONTEND_URL}/verify-email/{token}/"
+        # send_verification_email(instance, verification_url)
         
         # Also send OTP for email verification SYNCHRONOUSLY
         
