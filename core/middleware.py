f7ff5c1dfe70e9f274f4cb033aabd736d8883739
Shagor Robidas <roysagor88@gmail.com>
2025-09-11 05:28:43 +0600

    JWTAuthMiddleware

---
+++ core/middleware.py
@@ -0,0 +1,32 @@
+from urllib.parse import parse_qs
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@database_sync_to_async
+def get_user_from_token(token):
+    try:
+        UntypedToken(token)  # validate token
+        payload = UntypedToken(token).payload
+        user_id = payload.get("user_id")
+        return User.objects.get(id=user_id)
+    except (InvalidToken, TokenError, User.DoesNotExist):
+        return AnonymousUser()
+
+
+class JWTAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+
+    async def __call__(self, scope, receive, send):
+        query_params = parse_qs(scope["query_string"].decode())
+        token = query_params.get("token", [None])[0]
+        scope["user"] = await get_user_from_token(
+            token
+        ) if token else AnonymousUser()
+        return await self.inner(scope, receive, send)
