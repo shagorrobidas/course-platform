9fe342428626be4e93e862db98a73ca864412306
Shagor Robidas <roysagor88@gmail.com>
2025-09-11 05:29:55 +0600

    Notifications routing

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('users.api.urls')),
     path('course/', include('courses.api.urls')),
+    path('notifications/', include('notifications.routing')),
 ]
