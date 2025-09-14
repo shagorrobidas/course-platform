55f74bd1887bd4f7aab4e7d8b4b716054a8caee9
Shagor Robidas <roysagor88@gmail.com>
2025-09-14 06:27:48 +0600

    update

--- courses/api/serializers/__init__.py
+++ courses/api/serializers/__init__.py
@@ -3,7 +3,11 @@ from courses.api.serializers.lesson import LessonSerializer
 from courses.api.serializers.module import ModuleSerializer
 from courses.api.serializers.courses import CourseSerializer
 from courses.api.serializers.enrollment import EnrollmentSerializer
-from courses.api.serializers.progress import ProgressSerializer
+from courses.api.serializers.progress import (
+    ProgressSerializer,
+    ProgressCreateSerializer,
+    ProgressUpdateSerializer
+)
 
 
 __all__ = (
@@ -12,5 +16,7 @@ __all__ = (
     ModuleSerializer,
     CourseSerializer,
     EnrollmentSerializer,
-    ProgressSerializer
+    ProgressSerializer,
+    ProgressCreateSerializer,
+    ProgressUpdateSerializer
 )
